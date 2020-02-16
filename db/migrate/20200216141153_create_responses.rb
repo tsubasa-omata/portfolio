44a7bc02be8381d7456dd3820e6da826eda3e365
小俣翼 <runrunrun108@gmail.com>
2020-02-16 23:13:50 +0900

    response モデル作成

---
+++ db/migrate/20200216141153_create_responses.rb
@@ -0,0 +1,11 @@
+class CreateResponses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :responses do |t|
+      t.text :content
+      t.references :post, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
