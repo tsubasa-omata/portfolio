b58602864117764238df12e17b722d14361dff3f
小俣翼 <runrunrun108@gmail.com>
2020-02-17 13:31:04 +0900

    simple_formインストール

--- Gemfile
+++ Gemfile
@@ -41,6 +41,8 @@ gem 'bootstrap', '~> 4.1.1'
 gem 'jquery-rails'
 gem 'rails-i18n', '~> 5.1'
 gem 'devise'
+gem 'simple_form'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   devise_for :users
-  resources :users do
-    resources :threads, only: [:new, :create, :destroy ]
-  end
+  resources :users
+  resources :posts, only: [:new, :create, :destroy, :show, :index ]
 
-  resources :threads, only: [:create, :destroy, :create, :new, :show, :index] do
+  resources :posts, only: [:create, :destroy, :create, :new, :show, :index] do
     resources :responses, only: [:create, :destroy, ]
   end
-
-  
-
 end
