88546961e2b98a631dd5a4382c74f36f9c07e173
小俣翼 <runrunrun108@gmail.com>
2020-02-18 15:41:13 +0900

    post/show編集

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
-  has_many :post_category_relation
-  has_many :posts, through: :category_post_relation
+  has_many :post_category_relations
+  has_many :posts, through: :category_post_relations
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :responses
-  has_many :post_category_relation
-  has_many :categories, through: :post_category_relation
+  has_many :post_category_relations
+  has_many :categories, through: :post_category_relations
   validates :title, presence: true
   validates :content, presence: true
 end
